dec5d96bd6aa4f7691f9e1f8ce59c9e937704462
Rafael <rafael.martinezcalvo@orange.fr>
2023-09-22 21:17:15 +0200

    partial mol to svg

--- src/lib/mol.ts
+++ src/lib/mol.ts
@@ -1,6 +1,6 @@
 export async function molToSVG() {
-    const content =
-        `Methane
+  const content =
+    `Methane
   OpenBabel07080916093D
 
   5  4  0  0  0  0  0  0  0  0999 V2000
@@ -17,31 +17,43 @@ M  END
   `.split("\n").reverse();
 
   console.table(content)
-  
+
   const name = content.pop();
   content.pop();
   content.pop();
 
-  const [nAtoms, nBounds] = content.pop()?.trim().split(/\s+/).map(v=>parseInt(v)) || [0, 0, 0]
-  
+  const [nAtoms, nBounds] = content.pop()?.trim().split(/\s+/).map(v => parseInt(v)) || [0, 0, 0]
+
   console.log(name, nAtoms, nBounds);
 
 
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 
-
+  let xMin = Infinity;
+  let xMax = 0;
+  let yMin = Infinity;
+  let yMax = 0;
   for (let i = 0; i < nAtoms; i++) {
-    const [x,y,z, symbol] = content.pop()?.trim().split(/\s+/) as string[];
-    console.log(x,y,z,symbol)
+    const [xRaw, yRaw, zRaw, symbol] = content.pop()?.trim().split(/\s+/) as string[];
+    const [x, y, z] = [xRaw, yRaw, zRaw].map(n => parseFloat(n) * 10)
+    // console.log(x, y, z, symbol)
+
+    xMin = Math.min(xMin, x);
+    yMin = Math.min(yMin, y);
+    xMax = Math.max(xMax, x);
+    yMax = Math.max(yMax, y);
 
     //const element = document.createElement("text")
     const element = document.createElementNS("http://www.w3.org/2000/svg", "text");
-    element.setAttribute("x", (parseFloat(x) *10).toString());
-    element.setAttribute("y", (parseFloat(y) *10).toString());
-    element.textContent= symbol.toString();
+    element.setAttribute("x", x.toString());
+    element.setAttribute("y", y.toString());
+    element.setAttribute("font-size", (2 + (z / 10)).toString())
+    element.textContent = symbol.toString();
 
     svg.appendChild(element)
   }
+
+  svg.setAttribute("viewBox", [xMin - 2, yMin - 2, Math.abs(xMin) + xMax + 4, Math.abs(yMin) + yMax + 4].map(n => Math.round(n)).join(" "))
 
   console.log(svg.outerHTML)
 }
